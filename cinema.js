818748911215ab7c2d61ee44a87758e33ab61172
herrhermon <hermon.almeida.outlook.com>
2022-04-26 20:54:54 -0300

    Adicionado função listarFilmesEmCartaz

--- cinema.js
+++ cinema.js
@@ -14,3 +14,14 @@ function listarTodosOsFilmes(filmes) {
   }
   listarTodosOsFilmes(catalogoObj.data);
   
+
+  function listarFilmesEmCartaz(filmes) {
+    const filmesEmCartaz = filmes.filter((filme) => {
+      if (filme.emCartaz === true) {
+        return true;
+      }
+    });
+    return filmesEmCartaz;
+  }
+  console.log(listarFilmesEmCartaz(catalogoObj.data));
+  
